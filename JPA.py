90cd96ba6f08c4b8a10dfd9996188df003c2549d
Jack <jackwhershey@gmail.com>
2020-05-14 12:30:16 -0700

    Add files via upload

---
+++ JPA.py
@@ -0,0 +1,78 @@
+
+#whatever monster kills you takes your gear and exp just like a player would xp gain for kills is a % of opponents xp not 100% if you return and kill them you will get everything back but xp will be lost in transfer
+
+#Nothing will ever despawn if you die you respawn with nothing lvl 1 so do the mobs when they die they will spawn as well they can move and will fight and gain xp/items just like you. 
+
+#there will be scrap mobs just like the spawning blobs in agar.io that will be easily farmable and wont move/update the game
+
+#Ai adventures who are on the same mission as you and will also be rogue like and you can interact with them
+
+#Weapons/Armor are cards and skills are static. A turn is a combination of a weapon and a skill. There will be many different skills but you can only bring a limited number of skills (3?) into each battle which you can equip in inventory.
+
+#What does fusion add besides more cards to your hand? What does it do that other card games cant? defusion and fusion skills can only be used once per turn cycle (your turn + enemies) they are infinite like hero powers. Defusion is when you use one of your skills after an opponent plays something (the possible things that can be played on your turn are: nothing which can not be defused, one card, 2 cards playing 2 cards is known as a combination/combo, 1 card 1 skill known as a fusion) you can not play a skill by itself.
+
+import time
+import numpy as np
+import random
+import math
+import csv
+import msvcrt
+from Map import road
+from random import shuffle
+from graphics import *
+
+
+
+dx, dy = 10, 0
+key = 0
+
+win = GraphWin('Face', 800, 600) # give title and dimensions
+
+head = Circle(Point(400,500), 25) # set center and radius
+head.setFill("yellow")
+head.draw(win)
+dx, dy = 10, 0
+mcx, mcy = 400,500
+p = None
+line = None
+count_1 = 0
+
+while(1):
+	
+	k = win.checkKey()
+
+
+	if k == 'a':
+		head.move(-dx, dy)
+		mcx -= dx
+	elif k == 'd':
+		head.move(dx, dy)
+		mcx += dx
+	elif k == 'w':
+		head.move(dy,-dx)
+		mcy -= dx
+	elif k == 's':
+		head.move(dy,dx)
+		mcy += dx
+	p = win.checkMouse()
+
+	if(p != None):
+		if (line):
+			line.undraw()
+		print(p)
+		x = p.x
+		y = p.y
+		line = Line(Point(mcx,mcy), Point(x,y))
+		line.draw(win)
+		print(str(x) + ', ' + str(y))
+		count_1 = 0
+
+	count_1+=1
+	
+
+
+
+
+
+
+
